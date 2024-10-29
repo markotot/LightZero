21ce5e9251ab951abd201d13d0ab4bacfa892163
markotot <m.tot@qmul.ac.uk>
2024-10-29 16:48:25 +0000

    feat: added playground script for testing purposes

---
+++ zoo/atari/entry/playground.py
@@ -0,0 +1,17 @@
+import wandb
+import torch
+
+from zoo.atari.config.atari_muzero_rnn_fullobs_config import device
+
+if __name__ == "__main__":
+    if torch.cuda.is_available():
+        device = 'cuda'
+        print(f"Device 'cuda'")
+    else:
+        device = 'cpu'
+        print(f"Device: 'cpu'")
+
+    config = {'device': device}
+    with wandb.init(project="iris-muzero", name="iris-muzero-playground", config=config):
+        metrics = {"loss": 0.5, "accuracy": 0.6}
+        wandb.log(data=metrics)
