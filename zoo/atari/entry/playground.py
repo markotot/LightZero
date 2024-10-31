46cc98c70e5fb2fd76cdf1cb0d4c895976ed5c64
markotot <m.tot@qmul.ac.uk>
2024-10-31 15:25:48 +0000

    feat: fixed the packages

--- zoo/atari/entry/playground.py
+++ zoo/atari/entry/playground.py
@@ -1,7 +1,5 @@
-#import wandb
+import wandb
 import torch
-
-from zoo.atari.config.atari_muzero_rnn_fullobs_config import device
 
 if __name__ == "__main__":
     if torch.cuda.is_available():
@@ -11,7 +9,7 @@ if __name__ == "__main__":
         device = 'cpu'
         print(f"Device: 'cpu'")
 
-    # config = {'device': device}
-    # with wandb.init(project="iris-muzero", name="iris-muzero-playground", config=config):
-    #     metrics = {"loss": 0.5, "accuracy": 0.6}
-    #     wandb.log(data=metrics)
+    config = {'device': device}
+    with wandb.init(project="iris-muzero", name="iris-muzero-playground", config=config):
+        metrics = {"loss": 0.5, "accuracy": 0.6}
+        wandb.log(data=metrics)
