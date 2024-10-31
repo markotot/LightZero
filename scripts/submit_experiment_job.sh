4c4d0a1410ffa8abd2c5879cd50613932c8f4827
markotot <m.tot@qmul.ac.uk>
2024-10-31 14:44:05 +0000

    fix: testing python versions

--- scripts/submit_experiment_job.sh
+++ scripts/submit_experiment_job.sh
@@ -6,6 +6,6 @@
 #$ -j y
 #$ -o job_results
 
-module load python/3.8.5
+module load python/3.10.14
 
 apptainer run --env-file myenvs --env "JOB_TYPE=$JOB_TYPE,RUN_NAME=$RUN_NAME,START_ID=$START_ID,END_ID=$END_ID,SEED=$SGE_TASK_ID" containers/iris.sif
