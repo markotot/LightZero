b6858994637162dcc0d4aead2ddd70eaa32f7344
markotot <m.tot@qmul.ac.uk>
2024-11-12 10:54:09 +0000

    feat: added scripts to run it as a job on appocrita

--- checkpoints/download_checkpoints.py
+++ checkpoints/download_checkpoints.py
@@ -3,4 +3,6 @@ from huggingface_hub import snapshot_download
 repo_id="eloialonso/iris"
 local_dir="pretrained_iris"
 
+print(f"Downloading {repo_id} to {local_dir}")
 snapshot_download(repo_id=repo_id, local_dir=local_dir)
+print("Downloaded all checkpoints!")
