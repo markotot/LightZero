fda1d2b06efb7a07d9a02d0103cf3514ef81e21c
markotot <m.tot@qmul.ac.uk>
2024-11-12 14:07:01 +0000

    fix: added wait for all processes to finish

--- scripts/run_iris.sh
+++ scripts/run_iris.sh
@@ -5,7 +5,8 @@ set NUM_SEEDS [lindex $argv 0]
 set ENV_NAME [lindex $argv 1]
 
 for ((i=0; i<=NUM_SEEDS; i++)); do
-    python3 -m atari_eval_iris_model $i $environment &
+    echo "Evaluating seed $i on environment $ENV_NAME"
+    python3 -m atari_eval_iris_model $i $ENV_NAME &
 done
 
 wait
