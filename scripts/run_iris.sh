2081ceb34bfa25088b863c49fc33da2c1f098be7
markotot <m.tot@qmul.ac.uk>
2024-11-13 13:32:29 +0000

    fix: python path of apocrita

--- scripts/run_iris.sh
+++ scripts/run_iris.sh
@@ -1,21 +1,18 @@
 # Description: Run the evaluation of the Iris model on the Atari environment
 
-
-
-
-cd zoo/atari/entry/
+#cd zoo/atari/entry/
 NUM_SEEDS=$1
 ENV_NAME=$2
 WANDB_API_KEY=$3
 wandb login $WANDB_API_KEY
 
-export PYTHONPATH="/data/home/acw549/LightZero:$PYTHONPATH"
+#export PYTHONPATH="/data/home/acw549/LightZero:$PYTHONPATH"
 
 echo "Evaluating Iris model on $NUM_SEEDS seeds on environment $ENV_NAME"
 
 for ((i=0; i<NUM_SEEDS; i++)); do
     echo "Evaluating seed $i on environment $ENV_NAME"
-    python3 -m atari_eval_iris_model $i $ENV_NAME &
+    python3 -m zoo.atari.entry.atari_eval_iris_model $i $ENV_NAME &
 done
 
 wait
